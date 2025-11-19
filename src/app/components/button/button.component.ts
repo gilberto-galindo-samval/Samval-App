c35afca9d9aaf42dc3dfb0cbd8742570c0a21f99
gilberto-galindo-samval <gilberto.galindo@samval.org>
2025-11-19 09:27:41 -0500

    se añade un boton
    solo es visual

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.less'
 })
