3e63a022eada62917fd67d4a61eb8e9f0ae3253c
gilberto-galindo-samval <gilberto.galindo@samval.org>
2025-11-20 10:07:19 -0500

    cambio en enviroments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
 
   production: false,
   apiBaseUrl: 'http://google.com',
-  environmentName :'##ENV_NAME_PLACEHOLDER##'
+  environmentName :'Desarrollo'
 
 };
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'http://google.com/v1',
-  environmentName :'##ENV_NAME_PLACEHOLDER##'
+  environmentName :'Productivo'
 };
