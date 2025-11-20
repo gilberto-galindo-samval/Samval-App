27c2f27a49357b2be67fe1a223f2e9c3f7a91f9d
gilberto-galindo-samval <gilberto.galindo@samval.org>
2025-11-19 23:07:44 -0500

    se añade el ambiente

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-button',
@@ -8,5 +9,7 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './button.component.less'
 })
 export class ButtonComponent {
+
+  enviroment = environment.production
 
 }
